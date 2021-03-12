20b44eebf5ebead85bb60a96ec7e23ab1b6f0fa8
JANG-YEJI <jangyeji005@gmail.com>
2021-03-12 10:35:16 +0900

    JOIN 없어진 기록찾기

---
+++ FindMissingData.sql
@@ -0,0 +1,11 @@
+-- 프로그래머스 없어진 기록 찾기
+/*
+천재지변으로 인해 일부 데이터가 유실되었습니다.
+입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+*/
+
+SELECT ANIMAL_OUTS.ANIMAL_ID, ANIMAL_OUTS.NAME
+FROM ANIMAL_INS RIGHT OUTER JOIN ANIMAL_OUTS
+ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
+WHERE ANIMAL_INS.ANIMAL_ID IS NULL
+ORDER BY ANIMAL_OUTS.ANIMAL_ID
