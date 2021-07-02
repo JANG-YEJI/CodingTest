b8dc7b7c0bfdc616b6b012fa736101f1a7c6dde2
JANG-YEJI <jangyeji005@gmail.com>
2021-07-03 01:37:25 +0900

    interchange

--- Python/book/6-4_Interchange.py
+++ Python/book/6-4_Interchange.py
@@ -1,2 +1,22 @@
 n, k = map(int, input().split())
 
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+
+for i in range(n):
+    for j in range(n):
+        if a[i] < b[j]:
+            tmp = a[i]
+            a[i] = b[j]
+            b[j] = tmp
+            k += -1
+            if k == 0:
+                break
+    if k == 0:
+                break
+
+print(sum(a))
